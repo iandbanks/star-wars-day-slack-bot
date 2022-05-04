7aaed93ab70d04833f40680ea67b53606a976376
-e <iandavidbanks@gmail.com>
2022-05-04 10:08:52 -0500

    Add readme and updated app to accept new env vars

--- app.js
+++ app.js
@@ -26,25 +26,23 @@ const app = new App({
     //console.log('⚡️ Bolt app is running!');
 })();
 
-// Unix timestamp for tomorrow morning at 9AM
-const today = moment.tz('2022-05-03 14:20', 'America/Chicago').unix();
-//console.log(today);
+// ENV Vars
+const channelId = process.env.CHANNEL_ID; // Channel you want to post the message to
+const startDate = process.env.START_DATE;
+const scheduleOffset = process.env.SCHEDULE_OFFSET;
+
 const now = moment.tz("America/Chicago");
-const start = moment.tz("2022-05-04 8:40:00","Y-M-D H:m:s","America/Chicago");
-//console.log(start.unix());
+const start = moment.tz(startDate,"Y-M-D H:m:s","America/Chicago");
 
 
-// Channel you want to post the message to
-const channelId = "C029BEVUX";
 
-async function publishMessage(channelId, blocks, text, time, attempt) {
+async function publishMessage(blocks, text, time, attempt) {
 
     try {
         // Call the chat.scheduleMessage method using the WebClient
         const result = await app.client.chat.scheduleMessage({
             channel: channelId,
             text: text,
-            //text:"",
             blocks: blocks,
             // Time to post message, in Unix Epoch timestamp format
             post_at: time
@@ -52,14 +50,12 @@ async function publishMessage(channelId, blocks, text, time, attempt) {
 
         console.log(result);
         log.push(result);
-        //console.log(`${attempt}  | ${facts.length}`);
         if (attempt === facts.length) {
             exit();
         }
     } catch (error) {
         console.error(error);
         errors.push(error);
-        //console.log(`${attempt}  | ${facts.length}`);
         if (attempt === facts.length) {
             exit();
         }
@@ -68,17 +64,16 @@ async function publishMessage(channelId, blocks, text, time, attempt) {
 
 for (let i = 0; i < facts.length; i++) {
     let attempt = i + 1;
-    let time = start.add(20, "minutes");
+    let time = start.add(scheduleOffset, "minutes");
     let schedule = time.unix();
-    //publishMessage("C03DXGGA02W", facts[i].blocks, facts[i].text, schedule, attempt); // Test Channel
-    publishMessage("C029BEVUX", facts[i].blocks, facts[i].text, schedule, attempt); // Water Cooler
-    //console.log(facts[i],time.format('Y-m-d H:m:s'));
+
+    publishMessage( facts[i].blocks, facts[i].text, schedule, attempt); // Water Cooler
 
 }
 
 function exit() {
     //fs.writeFile('./log-'+ moment().tz("America/Chicago").format("Y-m-d-H-m-s")+'.json',log).then(()=>{
-    fs.writeFile('log'+now.unix()+'.json', JSON.stringify(log))
+    fs.writeFile('log-'+now.unix()+'.json', JSON.stringify(log))
         .then((response) => {
             process.exit();
         }).catch((e) => {
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Star Wars Day Bot
+A fun Slack App to spit out various Star Wars related content throughout the day on May 4th!
+
+## Installation / Set up
+### Environment Variables
+1. Create a `.env` file using the values from `.env-sample` as a starter.
+2. Get the [Slack Bot Token](https://api.slack.com/apps/A03EA4GSY6M/oauth?success=1#bot-token-container) and set it to the `SLACK_BOT_TOKEN` variable. [Screenshot](./readme/bot-token.png)
+3. Get the [Client Secret](https://api.slack.com/apps/A03EA4GSY6M/general?#change_secret_form_3486152916225) and set it to the `SLACK_SIGNING_SECRET` variable. [Screenshot](./readme/client-secret.png)
+4. Set the `START_DATE` variable.
+    * This is the date and time that you want the first message to post to Slack. 
+    * This must be in the format `Y-m-d H:m:s` or the function will not schedule the messages. 
+    * Example: Set to `2022-05-04 9:00:00` if you want to schedule messages to post on May 4th, 2022 starting at 9 am.
+5. Set the `CHANNEL_ID` variable.
+    * This is the ID of the specific channel you want this Slack App to post to. 
+    * You can get the channel ID of any channel by clicking on the channel name, then scrolling to the [bottom of the dialog box](./readme/channel-id.png).
+6. Set the `SCHEDULE_OFFSET` variable.
+    * This is the number of minutes between each message post.
+    * This must be a number.
+
+### Install
+1. Run `npm install`
+
+## Usage
+### Scheduling
+Once you have set everything up, you are ready to schedule the messages.
+1. Run either `node app.js` or `npm run schedule`
+2. Once successfully run, a log file will save in `./logs/log-{unix-timestamp}.log`
+    * Example: `./logs/log-1651641319.log`
